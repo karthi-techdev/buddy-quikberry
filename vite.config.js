40eb2206dd0cb974f09776f41ab726cbfd8360c1
karthi <kr748283@gmail.com>
2025-05-01 13:02:00 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: 'index.html'
-    }
-  },
   server: {
     historyApiFallback: true
   }
